9b2a50804042bf0abe3e38253ac8e2469e27cb8d
RayGervais <raygervais@hotmail.ca>
2018-09-29 13:08:36 -0400

    fix(ports): Fixes port-mismatch in example

--- Ray-Gervais-Way/run.sh
+++ Ray-Gervais-Way/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e 
-# ex: run.sh -u test02 -n node -p 8000 -l Desktop/docker-node-tutorial/Offical-Node-JS-Way
+# ex: run.sh -u test02 -n node -p 8000 -l /Desktop/docker-node-tutorial/Offical-Node-JS-Way
 while getopts u:n:p:l: option
 do
 case "${option}"
@@ -19,7 +19,7 @@ docker images
 
 # Determine OS and run Docker container with mounted volume
 if [[ $OSTYPE == "msys" ]]; then
-    docker run -v C:/Users/$(whoami)/$LOCATION:/usr/src/app -it -p $PORT:8000 $USER/node-base bash
+    docker run -v C:/Users/$(whoami)/$LOCATION:/usr/src/app -it -p $PORT:8080 $USER/node-base bash
 else 
     docker run -v $LOCATION:/app -it -p $PORT:8000 $USER/node-base
 fi
